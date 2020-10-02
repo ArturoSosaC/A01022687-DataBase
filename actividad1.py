be64ba5189791e164e0dbc08b73f4ab613315a98
iArturoSosaC <iA01022687@itesm.mx>
2020-10-02 13:59:33 -0500

    Añadir list.py

--- actividad1.py
+++ actividad1.py
@@ -1,8 +1,12 @@
+import numpy as np
 from io_utilities import load_data
+
 
 def main():
     data = load_data('./TextFiles/populationbycountry19802010millions.csv')
-    print (data)
-    
+    # print (data)
+    x = np.array([f[:-1] for f in data])
+    y = np.array([f[-1] for f in data])
+    print(y)
 
 main()
